89f70a19f8922acc0a9303103d008707ba4e1874
Alyaa Al-Ali <alyaa470@gmail.com>
2026-07-11 15:03:28 +0300

    https://modern-store-1-nthj.onrender.com

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,1 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://modern-store-1-nthj.onrender.com",
-});
-
-export default api;
+import axios from "axios"; const api = axios.create({ baseURL: "https://modern-store-1-nthj.onrender.com", }); export default api;
